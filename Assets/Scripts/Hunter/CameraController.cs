287801c62eb1c3f825259866b7719367f066fdcc
Lucien Morcrette <lucien.morcrette@gmail.com>
2022-08-15 16:13:31 +0200

    start

---
+++ Assets/Scripts/Hunter/CameraController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    
+    [SerializeField] float _speed;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+    }
+
+    void Update()
+    {
+        float rotateY = Input.GetAxis("CamY");
+        float rotateX = Input.GetAxis("CamX");
+        Vector3 newAngle = transform.eulerAngles + _speed * new Vector3(rotateY, rotateX, 0);
+        if ( newAngle.x >= 0 && newAngle.x <= 90 )
+        {
+            transform.eulerAngles = newAngle;
+        } 
+        else
+        {
+            transform.eulerAngles += _speed * new Vector3(0, rotateX, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Hunter/HunterController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HunterController : MonoBehaviour
+{
+    [SerializeField] float _float;
+
+    void Update()
+    {
+           
+    }
+}
