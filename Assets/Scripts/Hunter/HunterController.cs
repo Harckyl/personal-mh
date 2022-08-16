5b5882c8f56000cdf874cf1440a2de44962524ff
Lucien Morcrette <lucien.morcrette@gmail.com>
2022-08-16 15:35:36 +0200

    add animation to hunter

--- Assets/Scripts/Hunter/HunterController.cs
+++ Assets/Scripts/Hunter/HunterController.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class HunterController : MonoBehaviour
 {
+    [SerializeField] Animator _hunterAnimation;
     [SerializeField] float _float;
+    [SerializeField] Transform _cameraOrientation;
+    
+    bool _run;
 
     void Update()
     {
