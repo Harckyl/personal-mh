253f83330e908f0db1fc15b1f3e2afb0cbdc3b8f
Lucien Morcrette <lucien.morcrette@gmail.com>
2022-08-22 12:38:00 +0200

    clean hunter movement

--- Assets/Scripts/Hunter/HunterController.cs
+++ Assets/Scripts/Hunter/HunterController.cs
@@ -2,14 +2,38 @@ using UnityEngine;
 
 public class HunterController : MonoBehaviour
 {
-    [SerializeField] Animator _hunterAnimation;
-    [SerializeField] float _float;
+    [SerializeField] Transform _hunter;
+    [SerializeField] float _walkSpeed;
+    [SerializeField] float _runSpeed;
     [SerializeField] Transform _cameraOrientation;
-    
-    bool _run;
+    [SerializeField] CharacterController _characterController;
 
+    [SerializeField] float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    float _gravity = -9.81f;
+
+    Vector3 velocity;
+    
     void Update()
     {
-           
+        velocity.y += _gravity * Time.deltaTime;
+        _characterController.Move(velocity * Time.deltaTime);
+        //need to do a ground check to reset gravity's velocity to 0 when grounded
+
+        float forward = Input.GetAxis("Forward");
+        float lateral = Input.GetAxis("Lateral");
+
+        float speed = _walkSpeed;
+        if(Input.GetAxis("Run") > 0) { speed = _runSpeed; }
+
+        Vector3 direction = new Vector3(lateral, 0, forward).normalized;
+        //need to take in consideration camera orientation
+
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(_hunter.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        _hunter.rotation = Quaternion.Euler(0f, angle, 0f);
+
+        _characterController.Move(direction * speed * Time.deltaTime);
+
     }
 }
